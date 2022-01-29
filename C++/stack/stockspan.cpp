74879aa7b408544b3676a8b5976cf4f8f7718407
Shrey Chandna <84912943+ShreyChandna@users.noreply.github.com>
2022-01-29 22:43:26 +0530

    new files

---
+++ C++/stack/stockspan.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+stack<int> s;
+void printSpan(int arr[],int n)
+{  
+    s.push(0);
+    cout<<"1"<<" ";
+    for(int i=1;i<n;i++)
+    {
+    while(s.empty()==false&&(arr[s.top()]<=arr[i]))
+    s.pop();
+    int span=s.empty()? i+1:i-s.top();
+    cout<<span<<" ";
+    s.push(i);
+    }
+}
+int main()
+{
+   int arr[]={18,12,13,14,11,16};
+   int n=6;
+   printSpan(arr,n);
+   return 0;
+}
